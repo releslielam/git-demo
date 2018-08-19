f9faa770f211f1d953bed617c36f343e5f947439
releslielam <37494420+releslielam@users.noreply.github.com>
2018-08-19 17:25:50 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-    //demo1
+    //demo1 bala
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
